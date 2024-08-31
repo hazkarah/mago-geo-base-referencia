2858ae6be346230e23536103a0d7a4a1b6f6d360
TI Tecnomapas <ti@tecnomapas.com.br>
2024-08-31 04:45:42 +0000

    up

--- src/main/java/br/gov/go/mago/geobasereferencia/MagoGeoBaseReferenciaApplication.java
+++ src/main/java/br/gov/go/mago/geobasereferencia/MagoGeoBaseReferenciaApplication.java
@@ -8,34 +8,32 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.QueryLookupStrategy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import br.gov.go.mago.car.core.CoreApplication;
-import br.gov.go.mago.car.core.security.CarCoreSecuritySemadGO;
 import br.gov.go.mago.car.domain.CarCoreDomain;
 import br.gov.go.mago.geobasereferencia.config.ApplicationPropertiesConfig;
 
 
 
 @SpringBootApplication(exclude = { ElasticsearchRestClientAutoConfiguration.class, LiquibaseAutoConfiguration.class })
-@Import(value = { CoreApplication.class, CarCoreDomain.class, CarCoreSecuritySemadGO.class })
+@Import(value = { CoreApplication.class, CarCoreDomain.class})
 @EnableScheduling
 @EnableCaching
 @EnableConfigurationProperties( value = { ApplicationPropertiesConfig.class } )
-@EnableJpaRepositories(basePackages = {"br.gov.go.mago.car.api.repository.jpa"},repositoryImplementationPostfix = "Impl", queryLookupStrategy=QueryLookupStrategy.Key.USE_DECLARED_QUERY )
-@EnableElasticsearchRepositories(basePackages= {"br.gov.go.mago.car.api.repository.elasticsearch"}, repositoryImplementationPostfix = "Impl", queryLookupStrategy=QueryLookupStrategy.Key.USE_DECLARED_QUERY)
-public class GeoportalBaseReferenciaApplication {
+@EnableJpaRepositories(basePackages = {"br.gov.go.mago.geobasereferencia.jpa"},repositoryImplementationPostfix = "Impl", queryLookupStrategy=QueryLookupStrategy.Key.USE_DECLARED_QUERY )
+// @EnableElasticsearchRepositories(basePackages= {"br.gov.go.mago.geobasereferencia.repository.elasticsearch"}, repositoryImplementationPostfix = "Impl", queryLookupStrategy=QueryLookupStrategy.Key.USE_DECLARED_QUERY)
+public class MagoGeoBaseReferenciaApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(GeoportalBaseReferenciaApplication.class, args);
+        SpringApplication.run(MagoGeoBaseReferenciaApplication.class, args);
     }
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GeoportalBaseReferenciaApplication.class);
+        return application.sources(MagoGeoBaseReferenciaApplication.class);
     }
 
 }
